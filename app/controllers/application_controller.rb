b2305a93d28951b3bacfb2071540d81d458aa36c
Meck <yesmeck@gmail.com>
2014-05-08 12:41:11 +0800

    User sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :user_signed_in?, :current_user
+
+  def sign_in_as(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= if session[:user_id].present?
+                        User.find_by(id: session[:user_id])
+                      end
+  end
 end
