ba1361d7cf63689855a031bc133e817f7289c4e5
Meck <yesmeck@gmail.com>
2014-05-08 18:01:40 +0800

    more specs for todo model

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Todo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#complete!' do
+    it 'complete the todo' do
+      todo = create(:todo, completed: false)
+      expect {
+        todo.complete!
+      }.to change { todo.completed }.from(false).to(true)
+    end
+  end
+
+  describe '#uncomplete!' do
+    it 'uncomplete the todo' do
+      todo = create(:todo, completed: true)
+      expect {
+        todo.uncomplete!
+      }.to change { todo.completed }.from(true).to(false)
+    end
+  end
 end
