55ae7c53b4434b1c7ff337af9a314030aa85ad28
Meck <yesmeck@gmail.com>
2014-05-08 12:43:47 +0800

    config rails generator

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Todo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.assets false
+      g.helper false
+    end
   end
 end
